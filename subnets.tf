37424df2a27ee75012de0f9d514faa844f2a1d24
karthik <karthik.sujath@gmail.com>
2025-10-05 13:07:09 +0530

    Updated subnets

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 # Public Subnets
 resource "aws_subnet" "ecomm-sub-1" {
-  vpc_id                  = aws_vpc.a1.id
+  vpc_id                  = aws_vpc.e-comm.id
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-southeast-1a"
   map_public_ip_on_launch = true
@@ -11,7 +11,7 @@ resource "aws_subnet" "ecomm-sub-1" {
 }
 
 resource "aws_subnet" "ecomm-sub-2" {
-  vpc_id                  = aws_vpc.a1.id
+  vpc_id                  = aws_vpc.e-comm.id
   cidr_block              = "10.0.16.0/24"
   availability_zone       = "ap-southeast-1b"
   map_public_ip_on_launch = true
@@ -23,7 +23,7 @@ resource "aws_subnet" "ecomm-sub-2" {
 
 # Private Subnets
 resource "aws_subnet" "ecomm-sub-3" {
-  vpc_id            = aws_vpc.a1.id
+  vpc_id            = aws_vpc.e-comm.id
   cidr_block        = "10.0.128.0/24"
   availability_zone = "ap-southeast-1a"
 
@@ -33,7 +33,7 @@ resource "aws_subnet" "ecomm-sub-3" {
 }
 
 resource "aws_subnet" "ecomm-sub-4" {
-  vpc_id            = aws_vpc.a1.id
+  vpc_id            = aws_vpc.e-comm.id
   cidr_block        = "10.0.144.0/24"
   availability_zone = "ap-southeast-1b"
 
@@ -43,7 +43,7 @@ resource "aws_subnet" "ecomm-sub-4" {
 }
 
 resource "aws_subnet" "ecomm-sub-5" {
-  vpc_id            = aws_vpc.a1.id
+  vpc_id            = aws_vpc.e-comm.id
   cidr_block        = "10.0.160.0/24"
   availability_zone = "ap-southeast-1a"
 
@@ -53,7 +53,7 @@ resource "aws_subnet" "ecomm-sub-5" {
 }
 
 resource "aws_subnet" "ecomm-sub-6" {
-  vpc_id            = aws_vpc.a1.id
+  vpc_id            = aws_vpc.e-comm.id
   cidr_block        = "10.0.176.0/24"
   availability_zone = "ap-southeast-1b"
 
