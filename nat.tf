0b53cee5a1e135624509e2249886b727715cb46a
karthik <karthik.sujath@gmail.com>
2025-10-06 22:45:09 +0530

    Updated nat

--- nat.tf
+++ nat.tf
@@ -7,6 +7,7 @@ resource "aws_eip" "ecomm-az1-eip" {
 resource "aws_nat_gateway" "ecomm-az1-nat" {
   subnet_id     = aws_subnet.ecomm-fe-sub-az1.id
   allocation_id = aws_eip.ecomm-az1-eip.id
+  depends_on = [ aws_internet_gateway.e-comm ]
 
   tags = {
     Name = "ecomm-nat-az1"
@@ -22,6 +23,7 @@ resource "aws_eip" "ecomm-az2-eip" {
 resource "aws_nat_gateway" "ecomm-az2-nat" {
   subnet_id     = aws_subnet.ecomm-fe-sub-az2.id
   allocation_id = aws_eip.ecomm-az2-eip.id
+  depends_on = [ aws_internet_gateway.e-comm ]
 
   tags = {
     Name = "ecomm-nat-az2"
