98a9a2cef5bb8a127e661175e4d2e93887899938
karthik <karthik.sujath@gmail.com>
2025-10-08 01:49:32 +0530

    Updated dbsubnetgrp

--- dbsubnetgrp.tf
+++ dbsubnetgrp.tf
@@ -1,6 +1,6 @@
 # Database Subnet Group
 resource "aws_db_subnet_group" "ecomm-db-sub-grp" {
-  subnet_ids = [aws_subnet.ecomm-db-sub-az1, aws_subnet.ecomm-db-sub-az2]
+  subnet_ids = [aws_subnet.ecomm-db-sub-az1.id, aws_subnet.ecomm-db-sub-az2.id]
 
   tags = {
     Name = "ecomm-db-sub"
