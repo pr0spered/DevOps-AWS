c4f4a608a32b0187461c611a8a29d59b2f8b83f6
karthik <karthik.sujath@gmail.com>
2025-10-08 01:34:03 +0530

    Updated targetgrp

--- targetgrp.tf
+++ targetgrp.tf
@@ -6,9 +6,9 @@ resource "aws_lb_target_group" "ecomm-tg" {
   vpc_id   = aws_vpc.e-comm.id
 
   health_check {
-    healthy_threshold = 3
+    healthy_threshold   = 3
     unhealthy_threshold = 2
-    matcher = "200,301,302"
-    protocol = "HTTP"
+    matcher             = "200,301,302"
+    protocol            = "HTTP"
   }
 }
