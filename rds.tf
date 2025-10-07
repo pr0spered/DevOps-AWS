81be35141da45e1a806a3920c3914f6263a21e21
karthik <karthik.sujath@gmail.com>
2025-10-07 22:59:41 +0530

    Added rds

---
+++ rds.tf
@@ -0,0 +1,33 @@
+# Database Instance
+resource "aws_db_instance" "ecomm-db-inst" {
+  identifier                  = "ecomm-db"
+  allocated_storage           = 20
+  engine                      = "mysql"
+  engine_version              = "8.0"
+  instance_class              = "db.t3.medium"
+  manage_master_user_password = true
+  username                    = "admin"
+  db_subnet_group_name        = aws_db_subnet_group.ecomm-db-sub-grp.name
+  vpc_security_group_ids      = [aws_security_group.ecomm-sec-db.id]
+  skip_final_snapshot         = true
+  multi_az                    = true
+  backup_retention_period     = 7
+
+  tags = {
+    Name = "ecomm-db"
+  }
+}
+
+# Database Instance Replica
+resource "aws_db_instance" "ecomm-db-repl" {
+  identifier              = "ecomm-db-replica"
+  instance_class          = "db.t3.medium"
+  replicate_source_db     = aws_db_instance.ecomm-db-inst.identifier
+  availability_zone       = "ap-southeast-1b"
+  vpc_security_group_ids  = [aws_security_group.ecomm-sec-db.id]
+  backup_retention_period = 7
+
+  tags = {
+    Name = "ecom-db-replica"
+  }
+}
