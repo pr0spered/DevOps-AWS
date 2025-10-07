647bb8b9fc07b2c4ae4f00a0d206604d6e48d860
karthik <karthik.sujath@gmail.com>
2025-10-08 01:33:20 +0530

    Added targetgrp

---
+++ targetgrp.tf
@@ -0,0 +1,14 @@
+# Target Group
+resource "aws_lb_target_group" "ecomm-tg" {
+  name     = "ecomm-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.e-comm.id
+
+  health_check {
+    healthy_threshold = 3
+    unhealthy_threshold = 2
+    matcher = "200,301,302"
+    protocol = "HTTP"
+  }
+}
