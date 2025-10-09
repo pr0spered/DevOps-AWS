70fd6a6aa8daf0ed07b9d56bfcf742292085b20b
karthik <karthik.sujath@gmail.com>
2025-10-10 01:42:15 +0530

    Added autoscaling group for frontend

--- asg.tf
+++ asg.tf
@@ -1,20 +1,20 @@
-# Auto Scaling Group
-resource "aws_autoscaling_group" "ecomm-asg" {
-  name                = "ecomm-asg"
+# Auto Scaling Group for frontend
+resource "aws_autoscaling_group" "ecomm-asg-fe" {
+  name                = "ecomm-asg-fe"
   min_size            = 2
   max_size            = 5
   desired_capacity    = 2
   health_check_type   = "ELB"
-  vpc_zone_identifier = [aws_subnet.ecomm-be-sub-az1.id, aws_subnet.ecomm-be-sub-az2.id]
+  vpc_zone_identifier = [aws_subnet.ecomm-fe-sub-az1.id, aws_subnet.ecomm-fe-sub-az2.id]
 
   launch_template {
-    id      = aws_launch_template.ecomm-launch-temp.id
+    id      = aws_launch_template.ecomm-launch-temp-fe.id
     version = "$Latest"
   }
 
   tag {
     key                 = "Name"
-    value               = "ecomm-asg"
+    value               = "ecomm-asg-fe"
     propagate_at_launch = true
   }
 }
