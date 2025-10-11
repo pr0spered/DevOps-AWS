685ccb47cf1e25f9713736b1769f2c8c65c0f8be
karthik <karthik.sujath@gmail.com>
2025-10-11 22:11:15 +0530

    Added autoscaling group for backend

--- asg.tf
+++ asg.tf
@@ -24,3 +24,24 @@ resource "aws_autoscaling_attachment" "ecomm-asg-lb-attach" {
   autoscaling_group_name = aws_autoscaling_group.ecomm-asg-fe.name
   lb_target_group_arn    = aws_lb_target_group.ecomm-tg.arn
 }
+
+# Auto Scaling Group for backend
+resource "aws_autoscaling_group" "ecomm-asg-be" {
+  name                = "ecomm-asg-be"
+  min_size            = 2
+  max_size            = 5
+  desired_capacity    = 2
+  health_check_type   = "ELB"
+  vpc_zone_identifier = [aws_subnet.ecomm-be-sub-az1.id, aws_subnet.ecomm-be-sub-az2.id]
+
+  launch_template {
+    id      = aws_launch_template.ecomm-launch-temp-be.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ecomm-asg-be"
+    propagate_at_launch = true
+  }
+}
