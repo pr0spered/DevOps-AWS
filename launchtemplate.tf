f210ffab80dc198c3f78802d7c4a3e2a92ff6a26
karthik <karthik.sujath@gmail.com>
2025-10-11 21:39:50 +0530

    Added launch template for backend

--- launchtemplate.tf
+++ launchtemplate.tf
@@ -15,6 +15,23 @@ resource "aws_launch_template" "ecomm-launch-temp-fe" {
   }
 }
 
+# Launch Template for backend EC2 instances
+resource "aws_launch_template" "ecomm-launch-temp-be" {
+  image_id               = data.aws_ami.amazon-linux-2023.image_id
+  instance_type          = "t2.micro"
+  key_name               = "sing_01"
+  vpc_security_group_ids = [aws_security_group.ecomm-sec-be.id]
+
+  network_interfaces {
+    security_groups             = [aws_security_group.ecomm-sec-be.id]
+    associate_public_ip_address = false
+  }
+
+  tags = {
+    Name = "ecomm-launch-temp-be"
+  }
+}
+
 data "aws_ami" "amazon-linux-2023" {
   most_recent = true
   owners      = ["amazon"]
