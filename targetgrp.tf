0319c67e9fda86e64b91902bf8e240f7238d1215
karthik <karthik.sujath@gmail.com>
2025-10-12 22:11:04 +0530

    Added target group for backend

--- targetgrp.tf
+++ targetgrp.tf
@@ -1,6 +1,6 @@
-# Target Group
-resource "aws_lb_target_group" "ecomm-tg" {
-  name     = "ecomm-tg"
+# Target Group for frontend
+resource "aws_lb_target_group" "ecomm-tg-fe" {
+  name     = "ecomm-tg-fe"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.e-comm.id
@@ -14,6 +14,20 @@ resource "aws_lb_target_group" "ecomm-tg" {
 
 # Attaching Target Group to Load Balancer
 resource "aws_lb_target_group_attachment" "a1" {
-  target_group_arn = aws_lb_target_group.ecomm-tg.arn
+  target_group_arn = aws_lb_target_group.ecomm-tg-fe.arn
   target_id        = aws_lb.ecomm-alb-fe.id
+}
+
+# Target Group for backend
+resource "aws_lb_target_group" "ecomm-tg-be" {
+  name     = "ecomm-tg-be"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.e-comm.id
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    protocol            = "HTTP"
+  }
 }
