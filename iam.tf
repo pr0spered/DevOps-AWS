450d1c4af520deea183c928005a246fcbe76d5fa
karthik <karthik.sujath@gmail.com>
2025-10-13 10:32:06 +0530

    Created IAM role for EC2 instances

---
+++ iam.tf
@@ -0,0 +1,23 @@
+# IAM Role for EC2 instances
+resource "aws_iam_role" "ecomm-role" {
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecomm-role"
+  }
+}
+
+
+
+
