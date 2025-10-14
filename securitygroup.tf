2e149cd26e1063feca25752aef0348d317d094fb
karthik <karthik.sujath@gmail.com>
2025-10-14 22:21:40 +0530

    Added security group for backend ALB

--- securitygroup.tf
+++ securitygroup.tf
@@ -29,6 +29,37 @@ resource "aws_vpc_security_group_egress_rule" "ecomm-out-alb" {
   ip_protocol       = "-1"
 }
 
+# Security Group with inbound and outbound rules for backend Application Load Balancer
+resource "aws_security_group" "ecomm-sec-alb-be" {
+  vpc_id = aws_vpc.e-comm.id
+
+  tags = {
+    Name = "ecomm-sec-grp-alb-be"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecomm-https-alb-be" {
+  security_group_id = aws_security_group.ecomm-sec-alb-be.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecomm-http-alb-be" {
+  security_group_id = aws_security_group.ecomm-sec-alb-be.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecomm-out-alb-be" {
+  security_group_id = aws_security_group.ecomm-sec-alb-be.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 # Security Group with inbound and outbound rules for Bastion Host
 resource "aws_security_group" "ecomm-sec-bh" {
   vpc_id = aws_vpc.e-comm.id
