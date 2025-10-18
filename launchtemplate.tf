7467df9d4af6fcc4d309a06ff800d970da6ca4b7
karthik <karthik.sujath@gmail.com>
2025-10-18 23:18:12 +0530

    Attatched IAM role to EC2

--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,9 +1,13 @@
 # Launch Template for frontend EC2 instances
 resource "aws_launch_template" "ecomm-launch-temp-fe" {
-  image_id               = data.aws_ami.amazon-linux-2023.image_id
-  instance_type          = "t2.micro"
-  key_name               = "sing_01"
-  user_data = "${file("frontend.sh")}"
+  image_id      = data.aws_ami.amazon-linux-2023.image_id
+  instance_type = "t2.micro"
+  key_name      = "sing_01"
+  user_data     = file("frontend.sh")
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.ecomm-instance-profile.arn
+  }
 
   network_interfaces {
     security_groups             = [aws_security_group.ecomm-sec-fe.id]
@@ -17,9 +21,9 @@ resource "aws_launch_template" "ecomm-launch-temp-fe" {
 
 # Launch Template for backend EC2 instances
 resource "aws_launch_template" "ecomm-launch-temp-be" {
-  image_id               = data.aws_ami.amazon-linux-2023.image_id
-  instance_type          = "t2.micro"
-  key_name               = "sing_01"
+  image_id      = data.aws_ami.amazon-linux-2023.image_id
+  instance_type = "t2.micro"
+  key_name      = "sing_01"
 
   network_interfaces {
     security_groups             = [aws_security_group.ecomm-sec-be.id]
