e30f210a2ee279fac3b3697e775e98de8bf2182b
karthik <karthik.sujath@gmail.com>
2025-10-18 23:21:49 +0530

    Added output file Displays RDS endpoint and Load Balancer DNS

---
+++ output.tf
@@ -0,0 +1,11 @@
+# Display RDS endpoint
+output "rds_endpoint" {
+  value       = aws_db_instance.ecomm-db-inst.endpoint
+  description = "The endpoint of the RDS instance"
+}
+
+# Display Load Balancer DNS
+output "lb_dns" {
+  value       = aws_lb.ecomm-alb-fe.dns_name
+  description = "The DNS of the Load Balancer"
+}
