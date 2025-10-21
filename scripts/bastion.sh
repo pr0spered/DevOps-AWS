d0a2ede7f1f1bb8b0a2cd218523297e90f6dc2fa
karthik <karthik.sujath@gmail.com>
2025-10-21 23:40:35 +0530

    Added commands to bastion.sh make a directory syncs database files to the directory create a db in rds instance import sql file to db

--- scripts/bastion.sh
+++ scripts/bastion.sh
@@ -5,3 +5,15 @@ dnf update -y
 
 # install mysql
 dnf install -y mariadb105
+
+# make a directory to store database files
+mkdir -p /home/ec2-user/database
+
+# sync the S3 bucket database contents
+aws s3 sync s3://${s3_bucket}/database/ /home/ec2-user/database/ --delete
+
+# creates a database in the RDS instance
+mysql -h "${db_endpoint}" -P 3306 -u admin -p12345678 -e "CREATE DATABASE \`ecomm-data\`;"
+
+# import the sql file to the database
+mysql -h "${db_endpoint}" -P 3306 -u admin -p12345678 ecomm-data < /home/ec2-user/database/shopee.sql
