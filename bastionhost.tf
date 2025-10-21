a465a7640f595e9fa42612cabc70ad4a9bf2ce3c
karthik <karthik.sujath@gmail.com>
2025-10-21 23:27:24 +0530

    Attatched IAM role and added user data to bastion host

--- bastionhost.tf
+++ bastionhost.tf
@@ -6,6 +6,10 @@ resource "aws_instance" "ecomm-bastion" {
   vpc_security_group_ids      = [aws_security_group.ecomm-sec-bh.id]
   subnet_id                   = aws_subnet.ecomm-fe-sub-az1.id
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ecomm-instance-profile.name
+  user_data = base64encode(templatefile("${path.module}/scripts/bastion.sh", {
+    db_endpoint = aws_db_instance.ecomm-db-inst.address,
+  }))
 
   tags = {
     Name = "ecomm-bastion-host"
