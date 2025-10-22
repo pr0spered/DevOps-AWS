990efbb50e2f03361f445c1d441b9cc7c4160c44
karthik <karthik.sujath@gmail.com>
2025-10-22 19:43:08 +0530

    Added s3 bucket name to bash.sh

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -14,7 +14,7 @@ systemctl start httpd
 systemctl enable httpd
 
 # sync the S3 bucket contents (website files) to Apache web root
-aws s3 sync s3://val-01 /var/www/html --delete
+aws s3 sync s3://${s3_bucket} /var/www/html --delete
 
 # set proper permissions for Apache web root
 chmod -R 755 /var/www/html
