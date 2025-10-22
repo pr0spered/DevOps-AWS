bed4325acf28da66ab1327d34502f2fe52d3d139
karthik <karthik.sujath@gmail.com>
2025-10-22 19:12:56 +0530

    Added userdata and attatched iam role to backend EC2

--- launchtemplate.tf
+++ launchtemplate.tf
@@ -26,6 +26,15 @@ resource "aws_launch_template" "ecomm-launch-temp-be" {
   image_id      = data.aws_ami.amazon-linux-2023.image_id
   instance_type = "t2.micro"
   key_name      = "sing_01"
+  user_data = base64encode(templatefile("${path.module}/scripts/bash.sh", {
+    db_endpoint = aws_db_instance.ecomm-db-inst.address,
+    s3_bucket   = aws_s3_bucket.ecomm-bucket.id
+  }))
+
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.ecomm-instance-profile.arn
+  }
 
   network_interfaces {
     security_groups             = [aws_security_group.ecomm-sec-be.id]
