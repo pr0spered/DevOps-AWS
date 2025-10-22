537a43a37c3addce83d25c0ad0a9960e19b19f91
karthik <karthik.sujath@gmail.com>
2025-10-22 19:38:06 +0530

    Commented unwanted resources for E-Commerce deployment

--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,25 +1,25 @@
-# Launch Template for frontend EC2 instances
-resource "aws_launch_template" "ecomm-launch-temp-fe" {
-  image_id      = data.aws_ami.amazon-linux-2023.image_id
-  instance_type = "t2.micro"
-  key_name      = "sing_01"
-  user_data = base64encode(templatefile("${path.module}/scripts/bash.sh", {
-    s3_bucket = aws_s3_bucket.ecomm-bucket.id
-  }))
+# # Launch Template for frontend EC2 instances
+# resource "aws_launch_template" "ecomm-launch-temp-fe" {
+#   image_id      = data.aws_ami.amazon-linux-2023.image_id
+#   instance_type = "t2.micro"
+#   key_name      = "sing_01"
+#   user_data = base64encode(templatefile("${path.module}/scripts/bash.sh", {
+#     s3_bucket = aws_s3_bucket.ecomm-bucket.id
+#   }))
 
-  iam_instance_profile {
-    arn = aws_iam_instance_profile.ecomm-instance-profile.arn
-  }
+#   iam_instance_profile {
+#     arn = aws_iam_instance_profile.ecomm-instance-profile.arn
+#   }
 
-  network_interfaces {
-    security_groups             = [aws_security_group.ecomm-sec-fe.id]
-    associate_public_ip_address = true
-  }
+#   network_interfaces {
+#     security_groups             = [aws_security_group.ecomm-sec-fe.id]
+#     associate_public_ip_address = true
+#   }
 
-  tags = {
-    Name = "ecomm-launch-temp-fe"
-  }
-}
+#   tags = {
+#     Name = "ecomm-launch-temp-fe"
+#   }
+# }
 
 # Launch Template for backend EC2 instances
 resource "aws_launch_template" "ecomm-launch-temp-be" {
